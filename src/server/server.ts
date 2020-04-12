c13f96d97892c12dddcd75e12dde9fa155b78032
Jack Anderson <andersjack@gmail.com>
2020-04-11 18:41:49 -0600

    connect via ssl on production

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,6 +15,7 @@ const connection: ConnectionOptions = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  ssl: process.env.NODE_ENV === 'production',
   synchronize: false,
   logging: process.env.NODE_ENV !== 'production',
   entities: ['dist/db/entity/**/*.js'],
